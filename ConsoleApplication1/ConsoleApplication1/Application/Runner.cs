0042a88c8b01f6424c0b39c025788161679badbc
JoshuaKellogg8 <103067204+JoshuaKellogg8@users.noreply.github.com>
2024-02-20 16:11:54 -0500

    Initial commit
    Mess around with adding some simple console commands, and want to add Kanji objects with audio files and the correct symbol as well

---
+++ ConsoleApplication1/ConsoleApplication1/Application/Runner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace JpPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Kanji cn = new Kanji();
+            List<char> vowels = new List<char> { 'A','E','I','O','U' };
+            Random rand = new Random();
+
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+            player.Play();
+
+            for (;;)
+            {
+                var key = Console.ReadKey(true);
+
+                if (key.Key.Equals(ConsoleKey.Enter))
+                {
+                    Console.Clear();
+                    Console.WriteLine(vowels.ElementAt<char>(rand.Next(0,vowels.Count)));
+                }
+                if (key.Key.Equals(ConsoleKey.Escape))
+                {
+                    break;
+                }
+            }
+
+        }
+
+    }
+}
